328c0d5cf9770e60f542458ea12a7f12ce7e8ca0
joe <joe@martinclan.info>
2026-09-13 16:03:39 -0400

    added outputs to infra stack

--- packages/infra/iac/infra-stack.ts
+++ packages/infra/iac/infra-stack.ts
@@ -1,25 +1,36 @@
-import { Stack, type StackProps } from "aws-cdk-lib";
+import { type StackProps } from "aws-cdk-lib";
 import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
 import type { Construct } from "constructs";
+import { QuackStack } from "@quack/cdk";
 
 export interface InfraStackProps extends StackProps {
   vpcName: string;
 }
 
-export class InfraStack extends Stack {
+export class InfraStack extends QuackStack {
   constructor(scope: Construct, props: InfraStackProps) {
     super(scope, "QuackInfraStack", props);
 
-    new Vpc(this, "Vpc", {
+    const subnets = [
+      { name: "public", subnetType: SubnetType.PUBLIC },
+      { name: "protected", subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      { name: "private", subnetType: SubnetType.PRIVATE_ISOLATED },
+    ];
+
+    const vpc = new Vpc(this, "Vpc", {
       vpcName: props.vpcName,
       maxAzs: 2,
       natGateways: 1,
       restrictDefaultSecurityGroup: false,
-      subnetConfiguration: [
-        { name: "public", subnetType: SubnetType.PUBLIC },
-        { name: "protected", subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-        { name: "private", subnetType: SubnetType.PRIVATE_ISOLATED },
-      ],
+      subnetConfiguration: subnets,
     });
+
+    this.addStackOutput("VpcName", props.vpcName);
+    this.addStackOutput("VpcCidr", vpc.vpcCidrBlock);
+    for (const { name } of subnets) {
+      for (const subnet of vpc.selectSubnets({ subnetGroupName: name }).subnets) {
+        this.addStackOutput(subnet.node.id, subnet.ipv4CidrBlock);
+      }
+    }
   }
 }
