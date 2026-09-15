8581499d5fa0d8682dfc18f7d8d03bfe70277b3c
joe <joe@martinclan.info>
2026-09-15 13:00:02 -0400

    renamed auth files

--- packages/libs/shared/src/index.ts
+++ packages/libs/shared/src/index.ts
@@ -1,4 +1,3 @@
 export { config } from "./config.ts";
 export { parameters } from "./parameters.ts";
-export { TokenError } from "./verify-id-token.ts";
-export { verifyIdToken } from "./verify-id-token.ts";
+export { verifyIdToken, TokenError } from "./auth/verify-id-token.ts";
